69db319e3b8484c61ec820b965bb783e2340e53e
Comrad Aleksandr <aleksandras@fatmap.com>
2018-02-09 01:37:06 +0200

    ...

--- templates/common/include/glue/types.hpp
+++ templates/common/include/glue/types.hpp
@@ -37,7 +37,8 @@ namespace glue
 	using float16_t = std::uint16_t;
 	using float32_t = float;
 	using float64_t = double;
-	using uniform_location_t = int;
+	using uniform_location_t = std::int32_t;
+	using program_name_t = std::uint32_t;
 
   using debug_proc_t = void (GLUE_APIENTRY *)(enum_t, enum_t, std::uint32_t, enum_t, std::int32_t,const char *, const void *);
 	using amd_debug_proc_t = void (GLUE_APIENTRY *)(std::uint32_t, enum_t, enum_t, std::int32_t, const char *, void *);
